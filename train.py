03d6e9106c4ad136fced00680ab2d8c8c087efe9
alpoc <dwiebold7@gmail.com>
2023-11-26 13:12:31 -0700

    models training. Time to try inference.

--- train.py
+++ train.py
@@ -6,15 +6,18 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import os
+import gc
 
 
 
 def get_files(recording_base_dir):
     """
+    TODO: use glob instead for easier code comprehension
     Cannot load the image files into memory. Too large.
     :return: list of files, json keypress file
     """
-    video_frames = []
+    frame_dir_list = []
+    df_combined = pd.DataFrame()
 
     # recording_base_dir = "recordings"
     recordings = os.listdir(recording_base_dir)
@@ -24,13 +27,14 @@ def get_files(recording_base_dir):
         video_frame_dir = os.path.join(recording_dir, "video_images")
         frame_list = os.listdir(video_frame_dir)
 
-        frame_dir_list = [os.path.join(video_frame_dir, frame) for frame in frame_list]
+        frame_dir_list.extend([os.path.join(video_frame_dir, frame) for frame in frame_list])
 
         recording_file = 'inputs.csv'
         df = pd.read_csv(os.path.join(recording_dir, recording_file))
         df = df.drop(columns=df.columns[0], axis=1)
+        df_combined = pd.concat([df_combined, df])
 
-    return frame_dir_list, df
+    return frame_dir_list, df_combined
 
 
 def load_model_onto_gpu():
@@ -74,8 +78,14 @@ def train_in_batches(x_train, y_train, model, x_test, y_test):
                                                      save_weights_only=True,
                                                      verbose=1)
     # Have to find a good balance for system memory and VRAM
+    # 32 batch exceeds 24GB VRAM.
     gpu_batch = 16
-    memory_batch = 512
+    # System: 32GB ram, os uses 5GB
+    # 512 is a good match for leaving 2GB free but when laoding in a new batch is OOMs for some reason.
+    # 384 is about 5GB each batch but the initial load uses 10GB. Not sure why
+    memory_batch = 384
+    epochs = 8
+    loop_count = 0
 
     while len(x_train) > 0:
         current_x_train = []
@@ -89,23 +99,51 @@ def train_in_batches(x_train, y_train, model, x_test, y_test):
 
         if len(tf.config.list_physical_devices('GPU')):
             with tf.device("/GPU:0"):
-                model_hist = model.fit(current_x_train, np.array(current_y_train), epochs=8, batch_size=gpu_batch,
-                                       callbacks=[cp_callback])
+                if save_callbacks:
+                    model_hist = model.fit(current_x_train, np.array(current_y_train),
+                                           epochs=epochs, batch_size=gpu_batch,
+                                           callbacks=[cp_callback])
+                else:
+                    model_hist = model.fit(current_x_train, np.array(current_y_train),
+                                           epochs=epochs, batch_size=gpu_batch)
+
+        # python garbage collection was not working fast enough.
+        del current_x_train
+        del current_y_train
+        # Todo: look into tracemalloc. gc.collect works but it's not perfect.
+        gc.collect()
+
+        if loop_count >= 5:
+            tf.keras.saving.save_model(model, "models/current_model.keras", overwrite=True)
+            loop_count = 0
+        loop_count += 1
+
         print(f'remaining data: {len(x_train)}')
 
+    tf.keras.saving.save_model(model, "models/current_model.keras", overwrite=True)
 
-def test_gpu():
+
+def gpu_check():
     print('yum')
-    print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+    num_gpus = len(tf.config.list_physical_devices('GPU'))
+    print("Num GPUs Available: ", num_gpus)
     print(tf.test.is_built_with_cuda())
+    if not num_gpus:
+        print('only cpu training available. Remove gpu check to continue')
+        exit()
 
 if __name__ == "__main__":
-    # test_gpu()
-    # exit()
-
+    # gpu_check()
+    # callbacks are saved after each epoc. It's not great in our case since we're batching data into the RAM.
+    save_callbacks = False
     recording_directory = "/media/dj/Games Drive/Nerual_networks"
+    model_location = "/media/dj/Games Drive/Nerual_networks/models/current_model.keras"
 
     x, y = get_files(recording_directory)
+    # for i in range(len(x)):
+    #     print(f"x: {x[i]},   y{y.iloc[i]}")
+    # exit()
+
 
     x_train, x_test, y_train, y_test = train_test_split(x, y)
 
