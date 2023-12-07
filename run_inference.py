ca4820889366c9afdd7caee67a9f8724a9713ca3
alpoc <dwiebold7@gmail.com>
2023-12-07 04:31:50 -0700

    greyscale code for inference.

--- run_inference.py
+++ run_inference.py
@@ -10,7 +10,9 @@ import dxcam
 import time
 import keyboard
 from matplotlib import pyplot as plt
-
+import matplotlib.image as mpimg
+import cv2
+import pygame
 
 def validate_model(model, x_test, y_test):
     memory_batch = 256
@@ -23,7 +25,10 @@ def validate_model(model, x_test, y_test):
         for _ in range(memory_batch):
             if len(x_test) == 0:
                 break
-            current_x_train.append(img_to_array(load_img(x_test.pop(0))) / 255)
+            if grayscale:
+                current_x_train.append(img_to_array(load_img(x_test.pop(0), color_mode='grayscale')) / 255)
+            else:
+                current_x_train.append(img_to_array(load_img(x_test.pop(0))) / 255)
             current_y_train.append(y_test.pop(0))
 
         current_x_train = np.array(current_x_train)
@@ -43,7 +48,7 @@ def validate_model(model, x_test, y_test):
 
 def visualize_prediction(model, x_test, y_test):
     for i in range(len(x_test)):
-        single_x = img_to_array(load_img(x_test[i]))
+        single_x = img_to_array(load_img(x_test[i], color_mode="grayscale")) / 255
         # normally our datashape is (num_pictures, width, height, depth)
         # only one image will be (width, height, depth)
         single_x = single_x.reshape(-1, single_x.shape[0], single_x.shape[1], single_x.shape[2])
@@ -56,7 +61,6 @@ def inference_on_game(model):
     camera = dxcam.create(device_idx=0, output_idx=1)
     camera.start(region=(0, 0, 1280, 720), target_fps=30)
     model_run = False
-    current_keys = []
 
     print("ready for model to drive")
     while True:
@@ -69,48 +73,70 @@ def inference_on_game(model):
         if keyboard.is_pressed('f8'):
             # show screen for test
             frame = camera.get_latest_frame()
-            plt.imshow(frame, interpolation='nearest')
-            plt.show()
+            gray_image = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+            cv2.imshow("image", gray_image)
+            cv2.waitKey(0)
+            # plt.imshow(gray_image)
+            # plt.show()
         if model_run:
-            # inference on CPU takes too long.
-            # keyboard.release('w')
-            # keyboard.release('a')
-            # keyboard.release('s')
-            # keyboard.release('d')
-
             frame = camera.get_latest_frame()
-            frame = frame.reshape(-1, frame.shape[0], frame.shape[1], frame.shape[2])
-            predicted_keys = model.predict(frame)
+            if grayscale:
+                frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY) / 255
+                frame = frame.reshape(-1, frame.shape[0], frame.shape[1])
+            else:
+                frame = frame.reshape(-1, frame.shape[0], frame.shape[1], frame.shape[2])
+            # start_time = time.time()
+            if average_results:
+                prediction_1 = []
+                for _ in range(2):
+                    predicted_keys = model.predict(frame, verbose=0) / 2
+                    # print(predicted_keys)
+                    if len(prediction_1) == 0:
+                        prediction_1 = predicted_keys.copy()
+                predicted_keys = list(map(sum,zip(prediction_1, predicted_keys)))
+            else:
+                predicted_keys = model.predict(frame, verbose=0)
+            # print(f'inferent time {time.time() - start_time}')
             predicted_keys = predicted_keys[0].tolist()
             if predicted_keys[0] > 0.5:
                 keyboard.press('w')
             else:
                 keyboard.release('w')
+
             if predicted_keys[1] > 0.5:
                 keyboard.press('a')
             else:
                 keyboard.release('a')
+
             if predicted_keys[2] > 0.5:
                 keyboard.press('s')
             else:
                 keyboard.release('s')
+
             if predicted_keys[3] > 0.5:
                 keyboard.press('d')
             else:
                 keyboard.release('d')
-            print(predicted_keys)
+            # print(predicted_keys)
+        clock.tick_busy_loop(60)
 
 
 if __name__ == "__main__":
     # Turn on to run model.validate on testing data, otherwise run inference on live game.
-    validate = False
+    validate = True
+    grayscale = True
+    # run two inference steps and average the results together.
+    average_results = False
+
+    pygame.init()
+    clock = pygame.time.Clock()
 
     # The naming convention is 'my_model.keras'. Windows views that as a file instead of a directory.
     # Both the standard and custom naming conventions will be used when saving model during training.
     if path.exists(config.windows_testing_directory):
         x, y = get_files(config.windows_testing_directory)
         x_shape = img_to_array(load_img(x[0])).shape
-        model_location = path.join(config.windows_model_location, "current_model", "keras_model_dir")
+        model_location = path.join(config.windows_model_location, "current_model_cnn_lstm_25_epochs", "keras_model_dir")
         #model = tf.keras.models.load_model(model_location, custom_objects=None, compile=True)
     else:
         x, y = get_files(config.linux_testing_directory)
@@ -118,8 +144,12 @@ if __name__ == "__main__":
         # model = tf.keras.saving.load_model(model_location, custom_objects=None, compile=True, safe_mode=True)
     model = tf.keras.models.load_model(model_location, custom_objects=None, compile=True)
 
-    if validate:
-        validate_model(model, list(x), y.values.tolist())
-    else:
-        inference_on_game(model)
+    # model.summary()
+    # exit()
+
+    visualize_prediction(model, x, y)
+    # if validate:
+    #     validate_model(model, list(x), y.values.tolist())
+    # else:
+    #     inference_on_game(model)
 
