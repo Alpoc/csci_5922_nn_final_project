98622ac66ae3163f559073b53dc9d465f4b62212
Alpoc <dwiebold7@gmail.com>
2024-03-29 14:44:22 -0600

    concurrency "working"

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 from tensorflow.keras.preprocessing.image import img_to_array, load_img
 
+import build_models
 from build_models import build_cnn_model, build_cnn_lstm_model, build_conv_lstm
 from utils import get_files
 
@@ -9,7 +10,34 @@ import os
 import gc
 import config
 import time
+from threading import Thread
+import asyncio
 
+memory_x_batches = []
+memory_y_batches = []
+
+
+def load_images_into_memory(x_train, y_train):
+    full_x_train = x_train.copy()
+    full_y_train = y_train.copy()
+    while True:
+        if len(memory_x_batches) > 1:
+            # print("batch filled")
+            time.sleep(0.25)
+            continue
+        next_x_train = []
+        next_y_train = []
+        print("loading images into memory")
+        for _ in range(memory_batch):
+            if len(x_train) == 0:
+                x_train = full_x_train.copy()
+                y_train = full_y_train.copy()
+                break
+            next_x_train.append(img_to_array(load_img(x_train.pop(0), color_mode=config.color_mode)) / 255)
+            next_y_train.append(y_train.pop(0))
+        memory_x_batches.append(np.array(next_x_train))
+        memory_y_batches.append(np.array(next_y_train))
+        print("done loading into memory")
 
 def train_in_batches(x_train, y_train, model):
     """
@@ -21,6 +49,8 @@ def train_in_batches(x_train, y_train, model):
     :param y_test:
     :return:
     """
+
+
 
     checkpoint_path = "model_weights/cp.ckpt"
     checkpoint_dir = os.path.dirname(checkpoint_path)
@@ -39,23 +69,26 @@ def train_in_batches(x_train, y_train, model):
     full_y_train = y_train.copy()
     epochs_ran = 0
 
-    for _ in range(1000):
+    # loop = asyncio.get_event_loop()
+    # loop.create_task(load_images_into_memory(x_train, y_train))
+    # loop.run_forever()
+    thread = Thread(target=load_images_into_memory, args=(x_train, y_train))
+    thread.start()
+
+
+    for _ in range(5):
+        # load_images_into_memory()
         while len(x_train) > 0:
-            current_x_train = []
-            current_y_train = []
-            print("loading images into memory")
-            for _ in range(memory_batch):
-                if len(x_train) == 0:
-                    break
-                current_x_train.append(img_to_array(load_img(x_train.pop(0), color_mode=config.color_mode)) / 255)
-                current_y_train.append(y_train.pop(0))
+            batch_start_time = time.time()
 
             # current_x_train, current_y_train = tf.keras.utils.timeseries_dataset_from_array(current_x_train, current_y_train, sequence_length=gpu_batch)
 
+            while len(memory_x_batches) == 0:
+                pass
             # Datatypes must be np arrays. same as tf.stack().
-            current_x_train = np.array(current_x_train)
+            current_x_train = memory_x_batches.pop()
             # current_x_train = tf.expand_dims(current_x_train, axis=0)
-            current_y_train = np.array(current_y_train)
+            current_y_train = memory_y_batches.pop()
             print("moving images from RAM to VRAM")
             if len(tf.config.list_physical_devices('GPU')):
                 with tf.device("/GPU:0"):
@@ -68,10 +101,10 @@ def train_in_batches(x_train, y_train, model):
                                                epochs=epochs, batch_size=config.gpu_batch)
 
             # python garbage collection was not working fast enough.
-            del current_x_train
-            del current_y_train
-            # Todo: look into tracemalloc. gc.collect works but it's not perfect.
-            gc.collect()
+            # del current_x_train
+            # del current_y_train
+            # # Todo: look into tracemalloc. gc.collect works but it's not perfect.
+            # gc.collect()
 
             if loop_count >= 32:
                 print('saving off model')
@@ -80,7 +113,7 @@ def train_in_batches(x_train, y_train, model):
                 # TF reports epoch time, but it takes a while to load the data into Memory
                 batches_remaining = round(len(x_train) / memory_batch)
                 avg_batch_time = (current_time - start_time) / batches_processed / 60
-                print(f"estimated time remaining: {batches_remaining * avg_batch_time} minutes")
+                print(f"estimated time remaining: {batches_remaining * avg_batch_time} seconds")
                 print(f"epochs_ran: {epochs_ran}")
                 save_model(model)
                 loop_count = 0
@@ -88,6 +121,7 @@ def train_in_batches(x_train, y_train, model):
 
             batches_processed += 1
             print(f'{round(len(x_train) / memory_batch)} batches to go!')
+            print(f"batch time: {time.time() - batch_start_time}")
 
         print('saving model')
         save_model(model)
@@ -145,7 +179,7 @@ if __name__ == "__main__":
     # type of NEW model to build. choose one. If not new model arch will be loaded of existing
     lstm_and_cnn_model = False
     cnn = False
-    pure_lstm = True
+    pure_lstm = False
 
     # Load num images from storage to memory.
     memory_batch = 256
@@ -165,8 +199,11 @@ if __name__ == "__main__":
             keras_model = build_cnn_model(input_shape)
         elif pure_lstm:
             keras_model = build_conv_lstm(input_shape)
+        else:
+            keras_model = build_models.build_test_cnn_model(input_shape)
 
     else:
         model_location = os.path.join(config.linux_model_location, "current_model", "keras_model_dir")
         keras_model = tf.keras.saving.load_model(model_location, custom_objects=None, compile=True, safe_mode=True)
+    keras_model.summary()
     train_in_batches(list(x), y.values.tolist(), keras_model)
